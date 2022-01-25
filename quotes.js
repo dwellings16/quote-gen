51863bcb50693b4c06dc239d07dd435caf5a3d47
Dan Wellings <Wellindf@clarkson.edu>
2022-01-25 14:30:49 -0500

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,24 @@
+const quoteArr = [
+{
+  "line": "In real life, I assure you, there is no such thing as algebra.",
+  "movie": "-Fran Lebowitz"
+},{
+  "line": "A book is not supposed to be a mirror. It's supposed to be a door.",
+  "movie": "-Fran Lebowitz"
+},{
+  "line": "There is no such thing as inner peace. There is only nervousness and death.",
+  "movie": "-Fran Lebowitz"
+},{
+  "line": "Life is something that happens when you can't get to sleep.",
+  "movie": "-Fran Lebowitz"
+},{
+  "line": "I prefer dead writers because you don't run into them at parties.",
+  "movie": "-Fran Lebowitz"
+},{
+  "line": "My favorite animal is steak.",
+  "movie": "-Fran Lebowitz"
+},{
+  "line": "Your life story would not make a good book. Don’t even try.",
+  "movie": "-Fran Lebowitz"
+}
+]
